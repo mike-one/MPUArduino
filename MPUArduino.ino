d9db608d9e7619410074c831754af1ff4b2b609e
garciaromero <alejandro@unam.mx>
2015-04-17 18:04:04 -0500

    3ro

--- MPUArduino.ino
+++ MPUArduino.ino
@@ -18,9 +18,9 @@
 #include "I2Cdev.h"
 #include "MPU6050.h"
 #include <SPI.h>
-#include <boards.h>
+//#include <boards.h>
 //#include <RBL_nRF8001.h>
-#include <VirtualWire.h>        //Libreria RF
+//#include <VirtualWire.h>        //Libreria RF
 
 
 
@@ -47,6 +47,19 @@ int16_t gx1, gy1, gz1;
 //Variables de aceleración y giro del segundo MPU
 int16_t ax2, ay2, az2;
 int16_t gx2, gy2, gz2;
+int countGxmin1 = 0;
+int countGxmax1 = 0;
+int countGzmax1  = 0;
+int countGzmin1  = 0;
+int countGymax1  = 0;
+int countGymin1  = 0;
+int countGxmin2 = 0;
+int countGxmax2 = 0;
+int countGzmax2  = 0;
+int countGzmin2  = 0;
+int countGymax2  = 0;
+int countGymin2  = 0;
+
 
 bool banderaLecturaA = true;
 int  countPromedio = 0;
@@ -62,6 +75,7 @@ double  movimientoZ = 0;
 //Estado de los Botones
 int estadoBoton1 = 0;              // 1: Presionado, 0: liberado
 int estadoBoton2 = 0;              // 1: Presionado, 0: liberado
+
 /*
 //Carga de la Bateria
 int analogBateria = 0;             // Variable que almacena el valor ANALÓGICO (0 a 1023) de la entrada bateria
@@ -81,8 +95,8 @@ void setup() {
   //  ble_begin();  //Desactivamos BLE
   pinMode (pinBoton1, INPUT);
   pinMode (pinBoton2, INPUT);
-//  pinMode (pinConexionUSB, INPUT);
-//  pinMode (pinLedConexion, OUTPUT);
+  //  pinMode (pinConexionUSB, INPUT);
+  //  pinMode (pinLedConexion, OUTPUT);
   //    ble_set_name ("PODIUM");  //Desactivamos BLE
   Wire.begin();
   Serial.begin(9600);
@@ -90,9 +104,9 @@ void setup() {
   accelgyro2.initialize();
 
   //Inicializa RF
-//  vw_set_ptt_inverted(true);
-//  vw_set_tx_pin(RF);
-//  vw_setup(4000);// speed of data transfer Kbps
+  //  vw_set_ptt_inverted(true);
+  //  vw_set_tx_pin(RF);
+  //  vw_setup(4000);// speed of data transfer Kbps
 
 }
 
@@ -100,122 +114,221 @@ void setup() {
 
 void loop() {
   //    radio();
-  leerMapearAcelerometrosGiroscopios();
   leerEstadoBotones();
+  leerMapearAcelerometrosGiroscopio1();
+  leerMapearAcelerometrosGiroscopio2();
 
-if(gx1==0 || gx1 ==1000){
-    Serial.print("\t\tgx1:");
-    Serial.print(gx1);
-}
-if(gy1==0 || gy1 ==1000){
-    Serial.print("\t\tgy1:");
-    Serial.print(gy1);
-}
-if(gz1==0 || gz1 ==1000){
-    Serial.print("\t\tgz1:");
-    Serial.print(gz1);
-}
-if(gx1==0 || gx1 ==1000 || gy1==0 || gy1 ==1000 || gz1==0 || gz1 ==1000 )
-  Serial.print("\n"); 
-//    Serial.print("\t\tboton1:");
-//    Serial.println(estadoBoton1);
+  if (gx1 == 0 ) {
+    countGxmin1++;
 
 
+    if (countGxmin1 > 20) {
+      if (estadoBoton1 == 1)
+        Serial.println("1-Anti horario con boton");
+      else
+        Serial.println("1-Anti horario sin boton");
 
-/*
+      countGxmin1  = 0;
+      delay(200);
 
-  if ( (estadoBoton1 == 1) && (estadoBoton2 == 1)) {
-    
-    for (contador = 0; contador < 20; contador++) {
-      controller = "4"  ;
-      vw_send((uint8_t *)controller, strlen(controller));
-      vw_wait_tx(); // Wait until the whole message is gone
     }
-
   }
+  else if (gx1 == 10000) {
+    countGxmax1++;
 
+    if (countGxmax1 > 20 ) {
+
+      if (estadoBoton1 == 1)
+        Serial.println("1-Horario con boton");
+      else
+        Serial.println("1-Horario sin boton");
+      countGxmax1  = 0;
+      delay(200);
+
+    }
+  }
   else {
-    if ( (estadoBoton1 == 1) && (banderaLecturaA)) {
+    countGxmax1  = 0;
+    countGxmin1  = 0;
 
-      countPromedio ++;
+  }
 
-      countAy += ay1;
-
-      delay(50);
-
-      if (countPromedio == 20 ) {
+  if (gz1 == 0 ) {
+    countGzmin1++;
 
 
-        movimientoY = countAy / 20;
-        
-        banderaLecturaA = false;
+    if (countGzmin1 > 20) {
+      if (estadoBoton1 == 1)
+        Serial.println("1-Arriba con boton");
+      else
+        Serial.println("1-Arriba sin boton");
 
-        if (movimientoY > 8)     // Arriba
-          for (contador = 0; contador < 20; contador++) {
-            controller = "0"  ;
-            vw_send((uint8_t *)controller, strlen(controller));
-            vw_wait_tx(); // Wait until the whole message is gone
-          }
+      countGzmin1  = 0;
+      delay(200);
 
-        else if (movimientoY < 8 ) //Abajo
-
-          for (contador = 0; contador < 20; contador++) {
-            controller = "1"  ;
-            vw_send((uint8_t *)controller, strlen(controller));
-            vw_wait_tx(); // Wait until the whole message is gone
-          }
-
-
-      }
-    }
-    else if (estadoBoton1 == 0) {
-      banderaLecturaA = true;
-      countPromedio = 0;
-      countAy = 0;
-    }
-
-
-
-    if ( (estadoBoton2 == 1) && (banderaLecturaB)) {
-
-      countPromedioD ++;
-
-      countAz += az2;
-
-      delay(50);
-
-      if (countPromedioD == 20 ) {
-
-
-        movimientoZ = countAz / 20;
-        banderaLecturaB = false;
-
-        if (movimientoZ > 8) //Izquierda
-
-          for (contador = 0; contador < 20; contador++) {
-            controller = "2"  ;
-            vw_send((uint8_t *)controller, strlen(controller));
-            vw_wait_tx(); // Wait until the whole message is gone
-          }
-
-        else if (movimientoZ < 8 ) //Derecha
-
-          for (contador = 0; contador < 20; contador++) {
-            controller = "3"  ;
-            vw_send((uint8_t *)controller, strlen(controller));
-            vw_wait_tx(); // Wait until the whole message is gone
-          }
-
-      }
-    }
-    else if (estadoBoton2 == 0) {
-      banderaLecturaB = true;
-      countPromedioD = 0;
-      countAz = 0;
     }
   }
-  
-  */
+  else if (gz1 == 10000) {
+    countGzmax1++;
+
+    if (countGzmax1 > 20 ) {
+
+      if (estadoBoton1 == 1)
+        Serial.println("1-Abajo con boton");
+      else
+        Serial.println("1-Abajo sin boton");
+      countGzmax1  = 0;
+      delay(200);
+
+    }
+  }
+  else {
+    countGzmax1  = 0;
+    countGzmin1  = 0;
+
+  }
+
+  if (gy1 == 0 ) {
+    countGymin1++;
+
+
+    if (countGymin1 > 20) {
+      if (estadoBoton1 == 1)
+        Serial.println("1-Izquierda con boton");
+      else
+        Serial.println("1-Izquierda sin boton");
+
+      countGymin1  = 0;
+      delay(200);
+
+    }
+  }
+  else if (gy1 == 10000) {
+    countGymax1++;
+
+    if (countGymax1 > 20 ) {
+
+      if (estadoBoton1 == 1)
+        Serial.println("1-Derecha con boton");
+      else
+        Serial.println("1-Derecha sin boton");
+      countGymax1  = 0;
+      delay(200);
+
+    }
+  }
+  else {
+    countGymax1  = 0;
+    countGymin1  = 0;
+
+  }
+
+
+  if (gx2 == 0 ) {
+    countGxmin2++;
+
+
+    if (countGxmin2 > 20) {
+      if (estadoBoton2 == 1)
+        Serial.println("2-Anti horario con boton");
+      else
+        Serial.println("2-Anti horario sin boton");
+
+      countGxmin2  = 0;
+      delay(200);
+
+    }
+  }
+  else if (gx2 == 10000) {
+    countGxmax2++;
+
+    if (countGxmax2 > 20 ) {
+
+      if (estadoBoton2 == 1)
+        Serial.println("2-Horario con boton");
+      else
+        Serial.println("2-Horario sin boton");
+      countGxmax2  = 0;
+      delay(200);
+
+    }
+  }
+  else {
+    countGxmax2  = 0;
+    countGxmin2  = 0;
+
+  }
+
+  if (gz2 == 0 ) {
+    countGzmin2++;
+
+
+    if (countGzmin2 > 20) {
+      if (estadoBoton2 == 1)
+        Serial.println("2-Arriba con boton");
+      else
+        Serial.println("2-Arriba sin boton");
+
+      countGzmin2  = 0;
+      delay(200);
+
+    }
+  }
+  else if (gz2 == 10000) {
+    countGzmax2++;
+
+    if (countGzmax2 > 20 ) {
+
+      if (estadoBoton2 == 1)
+        Serial.println("2-Abajo con boton");
+      else
+        Serial.println("2-Abajo sin boton");
+      countGzmax2  = 0;
+      delay(200);
+
+    }
+  }
+  else {
+    countGzmax2  = 0;
+    countGzmin2  = 0;
+
+  }
+
+  if (gy2 == 0 ) {
+    countGymin2++;
+
+
+    if (countGymin2 > 20) {
+      if (estadoBoton2 == 1)
+        Serial.println("2-Izquierda con boton");
+      else
+        Serial.println("2-Izquierda sin boton");
+
+      countGymin2  = 0;
+      delay(200);
+
+    }
+  }
+  else if (gy2 == 10000) {
+    countGymax2++;
+
+    if (countGymax2 > 20 ) {
+
+      if (estadoBoton2 == 1)
+        Serial.println("2-Derecha con boton");
+      else
+        Serial.println("2-Derecha sin boton");
+      countGymax2  = 0;
+      delay(200);
+
+    }
+  }
+  else {
+    countGymax2  = 0;
+    countGymin2  = 0;
+
+  }
+
 }
 
 //************************************ Modulo de Radiofrecuencia  *************************************
@@ -249,25 +362,31 @@ void radio() {
 */
 //************************************ Acelerometros y Giroscopios ************************************
 
-void leerMapearAcelerometrosGiroscopios() {
+void leerMapearAcelerometrosGiroscopio1() {
 
   accelgyro1.getMotion6(&ax1, &ay1, &az1, &gx1, &gy1, &gz1);    // Lee valores en bruto del acelerómetro 1
-  accelgyro2.getMotion6(&ax2, &ay2, &az2, &gx2, &gy2, &gz2);    // Lee valores en bruto del acelerómetro 2
 
   // Las siguientes líneas convierte todos los valores que llega a dar el acelerómetro y giroscopio a valores entre 0 y 9
   ax1 = map(ax1, -32768, 32767, 0, 1000);
   ay1 = map(ay1, -32768, 32767, 0, 1000);
   az1 = map(az1, -32768, 32767, 0, 1000);
-  gx1 = map(gx1, -32768, 32767, 0, 1000);
-  gy1 = map(gy1, -32768, 32767, 0, 1000);
-  gz1 = map(gz1, -32768, 32767, 0, 1000);
+  gx1 = map(gx1, -32768, 32767, 0, 10000);
+  gy1 = map(gy1, -32768, 32767, 0, 10000);
+  gz1 = map(gz1, -32768, 32767, 0, 10000);
+}
 
+
+void leerMapearAcelerometrosGiroscopio2() {
+
+  accelgyro2.getMotion6(&ax2, &ay2, &az2, &gx2, &gy2, &gz2);    // Lee valores en bruto del acelerómetro 2
+
+  // Las siguientes líneas convierte todos los valores que llega a dar el acelerómetro y giroscopio a valores entre 0 y 9
   ax2 = map(ax2, -32768, 32767, 0, 1000);
   ay2 = map(ay2, -32768, 32767, 0, 1000);
   az2 = map(az2, -32768, 32767, 0, 1000);
-  gx2 = map(gx2, -32768, 32767, 0, 1000);
-  gy2 = map(gy2, -32768, 32767, 0, 1000);
-  gz2 = map(gz2, -32768, 32767, 0, 1000);
+  gx2 = map(gx2, -32768, 32767, 10000, 0);
+  gy2 = map(gy2, -32768, 32767, 10000, 0);
+  gz2 = map(gz2, -32768, 32767, 10000, 0);
 }
 
 
